57b22ad1b26f850413ffeba12ea0f0f87ed24bc8
BigDarkLord <148328599+BigDarkLord@users.noreply.github.com>
2023-10-18 12:25:04 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+  this.setTimeout(()=>{
+    const element = document.querySelector('[style="position: fixed; bottom: 0px; left: 0px; right: 0px; width: 728px; text-align: center; margin: 0px auto; padding: 0px; max-width: 100%; z-index: 9999;"]');
+    this.document.body.removeChild(element)
+  },2000)
+})
